42306a206bb443da1e713317f99891be9f8ac87f
AsumVictor <Iamasum369@gmail.com>
2023-03-16 22:33:28 +0000

    Added a success story

--- src/Pages/WelcomePage.jsx
+++ src/Pages/WelcomePage.jsx
@@ -3,15 +3,16 @@ import Lottie from "lottie-react";
 import Button, { ButtonOutline, Features } from "../Components/Utitlity";
 import WelcomeHeader from "../Components/Header";
 import HeroBg from "./../assets/Images/heroBg.png";
+import Story from "./../assets/Images/Story.png";
 import HeroBgMobile from "./../assets/Images/heroBgMobile.png";
 import Group from "./../assets/Images/cummunityPic.png";
 import Valinetine from "./../assets/Images/valentine.png";
 import FeaturesData from "../Components/Features";
 import ChatScreen from "../assets/Animations/Chatscreen.json";
 import VideoCall from "../assets/Animations/VideoChat.json";
+import "../css/WelcomePage.css";
 
 export default function WelcomePage() {
-  console.log(FeaturesData);
   return (
     <>
       <WelcomeHeader />
@@ -87,11 +88,13 @@ export default function WelcomePage() {
 
       <section className="flex w-full flex-auto justify-between flex-wrap bg-Pink600 mt-5 px-3 md:px-10 rounded-t-3xl">
         <div className="w-full md:w-5/12 flex flex-col justify-center">
-          <h3 className="text-3xl md:text-5xl mt-10 font-bold text-white px-2 text-center">Efficient Messaging</h3>
+          <h3 className="text-3xl md:text-5xl mt-10 font-bold text-white px-2 text-center">
+            Efficient Messaging
+          </h3>
           <p className="text-white mt-10 font-bold px-2">
             Recusandae placeat laudantium dolore ipsum sed, pariatur voluptate
-            excepturi nesciunt amet porro est vero, consectetur minima ea
-           elit. Iure eum at libero praesentium, facilis repellat tempore obcaecat
+            excepturi nesciunt amet porro est vero, consectetur minima ea elit.
+            Iure eum at libero praesentium, facilis repellat tempore obcaecat
           </p>
         </div>
         <div className="w-full md:w-6/12 flex mt-20 md:mt-0 flex-row justify-center items-center">
@@ -106,11 +109,13 @@ export default function WelcomePage() {
 
       <section className="flex w-full flex-col md:flex-row-reverse flex-wrap-reverse justify-between bg-white mt-5 px-3 md:px-10 rounded-t-3xl">
         <div className="w-full md:w-5/12 flex flex-col justify-center">
-          <h3 className="text-3xl md:text-5xl mt-10 font-bold text-Pink900 px-2 text-center">Real-time Video call</h3>
-          <p className="text-Pink900 mt-10 font-bold px-2">
+          <h3 className="text-3xl md:text-5xl mt-10 font-bold text-Pink900 px-2 text-center">
+            Real-time Video call
+          </h3>
+          <p className="text-PinkBlack900 mt-10 font-bold px-2">
             Recusandae placeat laudantium dolore ipsum sed, pariatur voluptate
-            excepturi nesciunt amet porro est vero, consectetur minima ea
-           elit. Iure eum at libero praesentium, facilis repellat tempore obcaecat
+            excepturi nesciunt amet porro est vero, consectetur minima ea elit.
+            Iure eum at libero praesentium, facilis repellat tempore obcaecat
           </p>
         </div>
         <div className="w-full md:w-6/12 flex mt-20 md:mt-0 flex-row justify-center items-center">
@@ -121,9 +126,46 @@ export default function WelcomePage() {
             className="-mt-20 "
           />
         </div>
-        
       </section>
 
+      <section className="flex w-full flex-col md:flex-row flex-wrap justify-between bg-white mt-5 px-3 md:px-10 rounded-t-3xl">
+        <div className="w-full md:w-5/12 flex flex-col justify-center">
+          <h3 className="text-3xl md:text-5xl mt-10 font-bold text-Pink900 px-2 text-center capitalize">
+            Add Stories and Reels
+          </h3>
+          <p className="text-PinkBlack900 mt-10 font-bold px-2">
+            Recusandae placeat laudantium dolore ipsum sed, pariatur voluptate
+            excepturi nesciunt amet porro est vero, consectetur minima ea elit.
+            Iure eum at libero praesentium, facilis repellat tempore obcaecat
+          </p>
+        </div>
+        <div className="w-full md:w-6/12 flex md:mt-0 flex-row justify-center items-center">
+          <img src={Story} alt="Story" />
+        </div>
+      </section>
+
+      <section className="flex w-full flex-col md:flex-row py-10 flex-wrap justify-between bg-Pink900 mt-5 px-3 md:px-10 rounded-t-3xl">
+        <div className="w-full md:w-5/12 flex flex-col justify-center">
+          <h3 className="text-3xl md:text-5xl mt-10 font-bold text-white px-2 text-center capitalize">
+            How I met Roseline
+          </h3>
+          <p className="mt-6 text-white font-medium">
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Illo
+            recusandae molestiae et eveniet, ullam excepturi temporibus, aperiam
+            voluptate atque veniam minima, qui asperiores magnam aut. Quo iure
+            aspernatur vel ducimus?
+          </p>
+          <p className="mt-6 text-white font-medium">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores
+            ipsum aliquam ea? Nulla quas blanditiis accusantium iure maiores
+            corporis ea, animi ipsam aliquid saepe, doloremque laborum assumenda
+            asperiores soluta illum!
+          </p>
+        </div>
+        <div className="successImg w-full md:w-4/12 flex mt-10 md:mt-0 flex-row justify-center items-center">
+          <img src="images/black-couple.png" alt="Black-couple" />
+        </div>
+      </section>
     </>
   );
 }
